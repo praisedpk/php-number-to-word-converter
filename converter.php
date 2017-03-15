25723aafac2d83f837b4168055541afdd8beac64
Hamid Sarfraz <praisedpk@gmail.com>
2017-03-15 09:24:05 +0500

    added dependencies

--- converter.php
+++ converter.php
@@ -76,3 +76,31 @@
         }
         return '';
     }
+    
+    function trim_all( $str , $what = NULL , $with = ' ' )
+    {
+        if( $what === NULL )
+        {
+            //  Character      Decimal      Use
+            //  "\0"            0           Null Character
+            //  "\t"            9           Tab
+            //  "\n"           10           New line
+            //  "\x0B"         11           Vertical Tab
+            //  "\r"           13           New Line in Mac
+            //  " "            32           Space
+
+            $what   = "\\x00-\\x20";    //all white-spaces and control chars
+        }
+
+        return trim( preg_replace( "/[".$what."]+/" , $with , $str ) , $what );
+    }
+    
+    function str_replace_last( $search , $replace , $str ) {
+        if( ( $pos = strrpos( $str , $search ) ) !== false ) {
+            $search_length  = strlen( $search );
+            $str    = substr_replace( $str , $replace , $pos , $search_length );
+        }
+        return $str;
+    }
+    
+    
